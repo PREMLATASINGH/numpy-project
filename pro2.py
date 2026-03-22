86e9db8b03c1b0ce4db2c282d550fb3ee968635f
PREMLATASINGH <56335900+PREMLATASINGH@users.noreply.github.com>
2026-03-22 16:25:17 -0400

    product

--- pro2.py
+++ pro2.py
@@ -12,3 +12,5 @@ df["Revenue"]=df["quantity"]*df["price"]
 print(df)
 total_revenue=np.sum(df["Revenue"])
 print("total revenue:",total_revenue)
+product_revenue=df.groupby("product")["Revenue"].sum()
+print(product_revenue)
