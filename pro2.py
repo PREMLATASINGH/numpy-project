acc4589d9f5eec4f10e0d3f037ef425460c34e6d
PREMLATASINGH <56335900+PREMLATASINGH@users.noreply.github.com>
2026-03-22 16:29:13 -0400

    daily revenue

--- pro2.py
+++ pro2.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pandas as pd
 data={
-    "data":["2026-01-01","2026-01-02","2026-01-03","2026-01-04","2026-01-05"]
+    "date":["2026-01-01","2026-01-02","2026-01-03","2026-01-04","2026-01-05"]
    , "product":["A","B","A","C","B"],
    "quantity":[2,1,3,2,4],
    "price":[100,200,100,300,200]
@@ -16,3 +16,5 @@ product_revenue=df.groupby("product")["Revenue"].sum()
 print(product_revenue)
 best_product=product_revenue.idxmax()
 print("best product:",best_product)
+daily_revenue=df.groupby("date")["Revenue"].sum()
+print(daily_revenue)
