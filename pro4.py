5466e312ceb8fd98d4942d7103692596a0524433
PREMLATASINGH <56335900+PREMLATASINGH@users.noreply.github.com>
2026-03-23 15:05:35 -0400

    added table

---
+++ pro4.py
@@ -0,0 +1,10 @@
+import numpy as np
+import pandas as pd
+data={
+    "customer":["A","B","C","A","B","D"],
+    "product":["laptop","phone","tablet","phone","laptop","taplet"],
+    "quantity":[1,2,1,3,1,2],
+    "price":[1000,500,300,500,1000,300]
+}
+df=pd.DataFrame(data)
+print(df)
