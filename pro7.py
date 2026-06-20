eeca0cd15de1a87d026f4c54d5bf8432468856e0
PREMLATASINGH <56335900+PREMLATASINGH@users.noreply.github.com>
2026-06-20 15:35:48 -0400

    table

---
+++ pro7.py
@@ -0,0 +1,12 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+data={
+    "product":["apple","phone","tablet","phone","laptop","taplet"],
+    "quantity":[1,2,1,3,1,2],
+    "price":[100,50,300,500,100,300],
+    "sales":[100,100,300,1500,100,600]
+
+}
+df=pd.DataFrame(data)
+print(df)
